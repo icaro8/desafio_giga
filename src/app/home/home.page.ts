84e41907a4caa00b753bcaf0db0c6b1df96eaa8d
icaro <icaro8899@gmail.com>
2022-11-02 20:03:07 -0300

    wip

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,9 +11,11 @@ import { ApiService } from '../services/api.service';
 export class HomePage {
   @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
-  form: any = { loaded: false, allLoaded: false, user:[], data:[] };
+  form: any = { loaded: false, allLoaded: false, user: [], data: [] };
   toast: ApiService = new ApiService();
   page = 1;
+  filter: string = 'all';
+
   constructor(
     private plt: Platform,
     private modalController: ModalController,
@@ -29,14 +31,13 @@ export class HomePage {
       api.show_toast = false;
       api.url = `api/?format=json&results=20&page=${page}`;
       this.form.data = this.form.data.concat(await api.request());
-      this.form.data.map(e=>{
-        e.results.map(e=>{
-          if(e.gender == 'male') e.gender ='Masculino';
-          if(e.gender == 'female') e.gender ='Feminino';
+      this.form.data.map(e => {
+        e.results.map(e => {
+          if (e.gender == 'male') e.gender = 'Masculino';
+          if (e.gender == 'female') e.gender = 'Feminino';
         });
         this.form.user = this.form.user.concat(e.results);
         console.log(e);
-        
       })
       this.form.loaded = true;
       console.log(this.form);
@@ -46,20 +47,41 @@ export class HomePage {
     }
   }
 
- async showUser(i: number){
-  console.log(this.form.user[i]);
-  const modal = await this.modalController.create({
-    component: UserPage,
-    componentProps: {
-      detail: this.form.user[i],
+  async showUser(i: number) {
+    console.log(this.form.user[i]);
+    const modal = await this.modalController.create({
+      component: UserPage,
+      componentProps: {
+        detail: this.form.user[i],
+      }
+    });
+    await modal.present();
+    let x = await modal.onWillDismiss();
+    if (x.data) {
+      x.data = x.data;
     }
-  });
-  await modal.present();
-  let x = await modal.onWillDismiss();
-  if (x.data) {
-    x.data = x.data;
   }
- }
+
+  async filterUser() {
+    // try {
+    //   var api = new ApiService();
+    //   api.show_toast = false;
+    //   api.url = `api/?format=json&results=20&page=1`;
+    //   this.form.data = this.form.data.concat(await api.request());
+    //   this.form.data.map(e => {
+    //     e.results.map(e => {
+    //       if (e.gender == 'male') e.gender = 'Masculino';
+    //       if (e.gender == 'female') e.gender = 'Feminino';
+    //     });
+    //     this.form.user = this.form.user.concat(e.results);
+    //     console.log(e);
+    //   })
+    //   this.form.loaded = true;
+    //   console.log(this.form);
+    // } catch (e) {
+
+    // }
+  }
 
   async loadData(event) {
     console.log(event);
